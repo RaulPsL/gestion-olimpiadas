a9394b23e1ed3dc856e52b6709c4b11320b9df09
raulpsl <raulparedesld@gmail.com>
2025-10-20 23:10:23 -0400

    update: tables, points with filter, add reports

---
+++ resources/js/forms/interfaces/CierreFaseForm.ts
@@ -0,0 +1,7 @@
+export type FormCierreFase = {
+  cierres: {
+    usuario_encargado_id: number | null,
+    usuario_evaluador_id: number | null,
+    fase_id: number,
+  }[];
+};
---
+++ resources/js/forms/interfaces/Notas.ts
@@ -0,0 +1,9 @@
+export type FormNotas = {
+  notas: {
+    nota_olimpista_id: number;
+    nota_fase_id: number;
+    estado_olimpista: string;
+    nota: number;
+    comentarios: string;
+  }[];
+};
